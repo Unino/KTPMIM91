c048cdac59524e50e54ceaadac155f6c1f3c2d39
phuong trang <phuongtrang06@gmail.com>
2021-10-27 15:30:29 +0700

    Them xu ly nut cong

--- Calculator/TinhToan.Designer.cs
+++ Calculator/TinhToan.Designer.cs
@@ -29,26 +29,27 @@ namespace Calculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtA
             // 
-            this.textBox1.Location = new System.Drawing.Point(98, 74);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(247, 26);
-            this.textBox1.TabIndex = 0;
+            this.txtA.Location = new System.Drawing.Point(98, 74);
+            this.txtA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(247, 26);
+            this.txtA.TabIndex = 0;
             // 
-            // textBox2
+            // txtB
             // 
-            this.textBox2.Location = new System.Drawing.Point(98, 114);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(247, 26);
-            this.textBox2.TabIndex = 0;
+            this.txtB.Location = new System.Drawing.Point(98, 114);
+            this.txtB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(247, 26);
+            this.txtB.TabIndex = 0;
             // 
             // button1
             // 
@@ -59,6 +60,15 @@ namespace Calculator
             this.button1.TabIndex = 1;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(98, 189);
+            this.txtKetQua.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(247, 26);
+            this.txtKetQua.TabIndex = 0;
             // 
             // TinhToan
             // 
@@ -66,8 +76,9 @@ namespace Calculator
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(426, 243);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "TinhToan";
             this.Text = "Tính toán";
@@ -78,8 +89,9 @@ namespace Calculator
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtKetQua;
     }
 }
