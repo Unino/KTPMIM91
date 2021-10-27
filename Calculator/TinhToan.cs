c19f49bc2f740a0257db89bff21a053b769082b6
phuong trang <phuongtrang06@gmail.com>
2021-10-27 14:58:34 +0700

    Update form

--- Calculator/TinhToan.cs
+++ Calculator/TinhToan.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class TinhToan : Form
     {
-        public Form1()
+        public TinhToan()
         {
             InitializeComponent();
         }
